ab1a0d2b7e3c4b9af391d25738474772e06cd6b9
Lucas Ruiz <deth_lucas@hotmail.com>
2023-10-17 13:38:46 -0300

    searchbar Event

--- dsfilter/src/components/ListingBody/index.tsx
+++ dsfilter/src/components/ListingBody/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Filter } from "../Filter";
 import { Listing } from "../Listing";
+import * as productService from "../../services/product-service";
+import { ProductDTO } from "../../models/product";
 
 export function ListingBody() {
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+  const [minValue, setMinValue] = useState(0);
+  const [maxValue, setMaxValue] = useState(Number.MAX_VALUE);
+
+  useEffect(() => {
+    productService.findByPrice(minValue, maxValue).then((response) => {
+      setProducts(response.data.content);
+    });
+  }, []);
+
+  function handleSearch(minValue, maxValue) {
+    setMinValue(minValue);
+    setMaxValue(maxValue);
+  }
+
   return (
     <>
-      <Filter></Filter>
+      <Filter onFilter={handleSearch}></Filter>
       <Listing></Listing>
     </>
   );
